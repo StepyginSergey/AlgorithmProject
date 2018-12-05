d26311f3c2d242485aaee04d0c809751ae3110af
Sergey Stepygin <stepygin.sergey@gmail.com>
2018-12-05 22:14:47 +0300

    Начало выполнения ДЗ 3

--- src/lesson_3/Main.java
+++ src/lesson_3/Main.java
@@ -1,7 +1,12 @@
 package lesson_3;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
+        Stack<Character> stack = new Stack<>();
+
+
 
     }
 }
---
+++ src/lesson_3/MyStack.java
@@ -0,0 +1,8 @@
+package lesson_3;
+
+public class MyStack<E> {
+
+    private int size;
+
+
+}
