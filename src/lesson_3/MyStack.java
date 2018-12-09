19d3494651b55d9ef221e0ca2e6e3912f83a05ae
Sergey Stepygin <stepygin.sergey@gmail.com>
2018-12-09 20:36:16 +0300

    Добавлены классы с четвертого урока

--- src/lesson_3/MyStack.java
+++ src/lesson_3/MyStack.java
@@ -1,8 +1,49 @@
 package lesson_3;
 
+import java.util.NoSuchElementException;
+
 public class MyStack<E> {
 
     private int size;
+    private Object[] stack = new Object[1];
 
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void resize(int capacity) {
+        Object[] temp = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = stack[i];
+        }
+        stack = temp;
+    }
+
+    public int getSize(){
+        return stack.length;
+    }
+
+
+    public void push(E element){
+        if(size == stack.length){
+            resize(2 * stack.length);
+        }
+
+        stack[size++] = element;
+    }
+
+    public E pop(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        E element = (E) stack[size - 1];
+        stack[size - 1] = null;
+        size--;
+        if(size > 0 && size == stack.length/4){
+            resize(stack.length / 2);
+        }
+
+        return element;
+    }
 
 }
